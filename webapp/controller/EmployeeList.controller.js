16c1d9bab9d8d421d558be90131f324583c6769c
Dane Mark Abad <dane-mark.abad-ext@vinci-construction.com>
2025-06-06 10:31:50 +0000

    addfunction

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -5,6 +5,27 @@ sap.ui.define([
 
     return Controller.extend("sapips.training.employeeapp.controller.EmployeeList", {
         onInit() {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteEmployeeList").attachPatternMatched(this.onRouteMatched, this);
+        },
+        _fetchEmployeeCount: function () {
+            var oModel = this.getOwnerComponent().getModel();
+
+            oModel.refresh(true);
+            // Read the total count from the /EMPLOYEE endpoint using $count
+            oModel.read("/EMPLOYEE/$count", {
+                success: function (oData) {
+                    // Set the total count of employees in the model
+                    var oCountModel = new JSONModel({ employeeCount: oData });
+                    this.getView().setModel(oCountModel, "employeeCountModel");
+                }.bind(this),
+                error: function () {
+                    MessageToast.show("Error fetching employee count.");
+                }
+            });
+        },
+        _onRouteMatched: function(){
+            this._fetchEmployeeCount();
         }
     });
 });
